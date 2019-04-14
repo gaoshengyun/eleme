a20e3133e156db421e739f9ee879182937bad2b6
gaoshengyun <18616242909@163.com>
2019-04-14 14:51:15 +0800

    城市搜索,搜索历史,搜索历史的本地存储

--- src/views/city.vue
+++ src/views/city.vue
@@ -8,7 +8,7 @@
     </mt-header>
 
     <div class="mgtop40 padlr10 bgfff padbot10">
-      <input type="text" v-model="inputval" class="cityinput" placeholder="输入商务楼,学校,地址">
+      <input type="text" v-model="inputval" required class="cityinput" placeholder="输入商务楼,学校,地址">
       <div class="submit bgcol ih40" @click="searchcity">提交</div>
     </div>
 
@@ -16,23 +16,23 @@
 
       <div class="his after" v-show="list.length==0">
         <div class='maintop fs0-8 padlr10'>搜索历史</div>
-        <div class="mainbody bgfff ">
-          <div class="pad10 after hisitem">
-              <div class="ih30">南开区公园</div>
-              <div class="ih30 fs0-8 col9f">天津市南开区金马路112号</div>
+        <div v-show="history.length!==0" class="mainbody bgfff ">
+          <div v-for="(item, index) in history" :key="index" class="pad10 after hisitem">
+              <div class="ih30">{{item.name}}</div>
+              <div class="ih30 fs0-8 col9f">{{item.address}}</div>
           </div>
-          <div class="pad10 after hisitem">
-              <div class="ih30">南开区公园</div>
-              <div class="ih30 fs0-8 col9f">天津市南开区金马路112号</div>
-          </div>
-          <div class="clearall ih30 pad10 col9f">
+          <div @click="removehistory" class="clearall ih30 pad10 col9f">
               清空所有
           </div>
         </div>
       </div>
 
       <div class='search bgfff his' v-show="list.length!== 0"> 
-          <div class="pad10 after hisitem" v-for="(item, index) in list" :key="index">
+          <div 
+          class="pad10 after hisitem" 
+          v-for="(item, index) in list" :key="index" 
+          @click="goadress({name:item.name,latitude:item.latitude,longitude:item.longitude,address:item.address,geohash:item.geohash})"
+          >
               <div class="ih30">{{item.name}}</div>
               <div class="ih30 fs0-8 col9f">{{item.address}}</div>
           </div>
@@ -44,22 +44,60 @@
   </div>
 </template>
 <script>
+import {Toast} from 'mint-ui'
 export default {
   data() {
     return {
       list:[],
-      inputval:''
+      inputval:'',
+      history:[]
+    }
+  },
+  mounted() {
+    if(localStorage.getItem('history')){
+      this.history =JSON.parse(localStorage.getItem('history'))
+      //alert(typeof this.history)
     }
   },
   methods: {
     searchcity(){
-      alert(this.inputval)
+      if(this.inputval === ''){
+        Toast('请输入地址')
+        return fase;
+      }
       this.axios.get(`https://elm.cangdu.org/v1/pois?city_id=${this.$store.state.currentcity.id}&keyword=${this.inputval}&type=search`).then(result=>{
          if(result.status === 200){
-          //console.log(result.data)
+          console.log(result.data)
           this.list = result.data
+          if(result.data.length === 0){
+            Toast('抱歉,没有相关地址')
+          }
+          
          }
       })
+    },
+    goadress(e){
+      var arr = []
+      if(localStorage.getItem('history')){
+        arr = JSON.parse(localStorage.getItem('history'))
+        for(let i = 0;i < arr.length;i++){  //判断搜索地址是否重复
+          if(arr[i].geohash == e.geohash){
+            var isok = true;
+          }
+        }
+        if(!isok){
+          arr.push(e)
+        }
+      }else{
+        arr.push(e)
+      }
+      localStorage.setItem('history',JSON.stringify(arr))
+      this.history = JSON.parse(localStorage.getItem('history'))
+      this.list = []
+    },
+    removehistory(){
+      localStorage.removeItem('history')
+      this.history = []
     }
   },
 }
